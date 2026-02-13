67b4b496243208c1b80f7dfaf5f3d239fc15021d
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-02-13 01:28:12 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'morphframe-v1.0.6'; // INCREMENT THIS TO FORCE UPDATE
+const CACHE_VERSION = 'morphframe-v1.0.7'; // INCREMENT THIS TO FORCE UPDATE
 const FILES_TO_CACHE = [
     './',
     './index.html',
